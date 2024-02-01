b32d9cce3a2a5fcf12e5924a1a7fa899f0c3b2db
Meik Behr <meik.behr@gmx.de>
2024-02-01 20:41:39 +0100

    starting root work - show 1 pokemon using fetch

--- js/main.js
+++ js/main.js
@@ -1,11 +1,46 @@
 "use strict";
 
-/* sofern im HTML steht:
-	<body onload="init()"> 
+let currentPokemon;
 
 
-muss hier rein:
+function init() {
+	loadPokemon();
+}
 
-function init() {}
 
+async function loadPokemon() {
+	let url = `https://pokeapi.co/api/v2/pokemon/1`;
+	let response = await fetch(url);
+	currentPokemon = await response.json();
+	console.log(currentPokemon);
+
+	renderPokemonInfo();
+}
+
+
+function renderPokemonInfo() {
+	document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+
+	const pekomonImage = currentPokemon['sprites']['other']['dream_world']['front_default'];
+	document.getElementById('pokemonPic').src = pekomonImage;
+}
+
+
+
+
+
+
+
+
+
+
+
+/* 
+API
+
+pokeapi.co
+
+
+Infos:
+https://pokeapi.co/docs/v2#pokemon-section
 */
